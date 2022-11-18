4bd11593e1836d3b451628679b5b0f6914c2998c
mgrimm21 <mgrimm0021@live.com>
2022-11-18 03:12:05 -0500

    fixed spritesheet to actually populate

--- GEFinal/src/com/mgrimm21/gefinal/SpriteSheet.java
+++ GEFinal/src/com/mgrimm21/gefinal/SpriteSheet.java
@@ -9,21 +9,28 @@ import javax.imageio.ImageIO;
 public class SpriteSheet {
 	
 	private BufferedImage image;
-	private BufferedImage[][] sprites;
+	private BufferedImage[] sprites;
 	
 	public SpriteSheet(String fileName, int spriteWidth, int spriteHeight) {
-		File file = new File(fileName);
+		File file = new File("res/sprites/sheets/" + fileName);
 		try {
 			image = ImageIO.read(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		sprites = new BufferedImage[image.getWidth()/spriteWidth][image.getHeight()/spriteHeight];
+		int width = image.getWidth()/spriteWidth;
+		int height = image.getHeight()/spriteHeight;
+		sprites = new BufferedImage[width*height];
+		int i = 0;
+		for (int x = 0; x < image.getWidth(); x+= spriteWidth) {
+			for (int y = 0; y < image.getHeight(); y += spriteHeight) {
+				sprites[i++] = image.getSubimage(x, y, spriteWidth, spriteHeight);
+			}
+		}
 	}
 	
-	public BufferedImage getSprite(int x, int y) {
-		return sprites[x][y];
+	public BufferedImage getSprite(int i) {
+		return sprites[i];
 	}
 	
 }
